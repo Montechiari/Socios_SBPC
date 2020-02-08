58dbd68bafe085ceb018547f9f91ea60d12130bf
Montechiari <g.monte.silva@gmail.com>
2020-02-08 00:39:05 -0300

    aparentemente funcionando, falta metodos para busca e integrar melhor o SQL com o pandas

--- socios.py
+++ socios.py
@@ -18,23 +18,10 @@ class Socio():
 
 def gera_banco_de_dados():
     data_frame_socios = pd.read_csv(ENDERECO + ARQUIVO_DADOS_CSV, header=0)
-    campos = ", ".join(data_frame_socios.columns.values)
-
     banco_de_dados = sqlite3.connect(ENDERECO + NOME_BANCO, uri=False)
-    # meu_cursor = banco_de_dados.cursor()
-    # print(campos)
-    # meu_cursor.execute(f"create table socios ({campos})")
-
     with banco_de_dados:
         data_frame_socios.to_sql("socios", banco_de_dados)
-
-    print("fim!")
-
-#
-# def carrega_instrucoes():
-#     with open(ENDERECO + "gerador_SQL", "r") as arquivo:
-#         instrucoes = arquivo.read()
-#     return instrucoes
+    return banco_de_dados
 
 
 if __name__ == '__main__':
@@ -42,6 +29,12 @@ if __name__ == '__main__':
         arquivo = ENDERECO + NOME_BANCO
         conexao = sqlite3.connect(f"file:{arquivo}?mode=rw", uri=True)
     except sqlite3.OperationalError:
-        gera_banco_de_dados()
+        conexao = gera_banco_de_dados()
     except Exception as e:
         print(e)
+
+    meu_cursor = conexao.cursor()
+    meu_cursor.execute("SELECT * FROM socios WHERE anuidade > 2018")
+
+    for entrada in meu_cursor.fetchall():
+        print(entrada)
